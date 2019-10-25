6a4c48bc3c3103f37ada0d936f50e3e91034f8b2
korney3 <koren.iz3x@yandex.ru>
2019-10-25 13:10:33 +0300

    inception score

---
+++ source/is/Inception_Score_function.py
@@ -0,0 +1,41 @@
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+import torch.nn.functional as F
+
+def Inseption_Score(images_set, model_predictor, device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu"), n=10):
+    """
+    Calculates Inception score for images, created with some generator
+
+    Parameters
+    ----------
+    images_set : Torch Dataset
+        Dataset that returns generated image
+    model_predictor : PyTorch model
+        Model that extract feature vector of the image for calculating KL divergence
+    n : int
+        Number of splits of images set
+    Returns
+    -------
+    IS : float
+        Mean of IS score
+    IS_err : float
+        Error of IS score 
+    """
+    from math import floor
+    scores = []
+    batch_size = floor(len(images_set)/ n)
+    test_loader = DataLoader(images_set,batch_size =batch_size,shuffle=False)
+    for i,data in enumerate(test_loader):
+        data=data.to(device)
+        with torch.no_grad():
+            logits,_ = model_predictor(data)
+            p_yx = F.softmax(logits, dim = 1).cpu().numpy()
+        p_y = np.expand_dims(p_yx.mean(axis=0), 0)
+        KL = ((p_yx * (np.log(p_yx + 1e-6) - np.log(p_y + 1e-6))).sum(axis=1)).mean()
+        IS = np.exp(KL)
+        scores.append(IS)
+
+    IS = np.mean(scores)
+    IS_err = np.std(scores)
+    return IS, IS_err
